8d6a9c134781b1e82f539e82eea6344156d2be9e
Sri Krishna <112201013@smail.iitpkd.ac.in>
2025-09-19 14:32:26 +0530

    feat: added frontend starter code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import dashboard from "./components/dashboard/page";
+import AppRoutes from "./Routes";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    
+      <AppRoutes/>
+    
+  );
+}
+
+export default App;
