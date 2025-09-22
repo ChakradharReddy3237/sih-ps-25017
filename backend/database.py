3a992f5bd154d0d1d4fba106abd7ae4f0701c383
G_Bhavani_Shankar <112201026@smail.iitpkd.ac.in>
2025-09-22 11:47:51 +0530

    Changed the database config

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-password = os.getenv("PASSWORD")
-project_ref = os.getenv("PROJECT_REF")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-DATABASE_URL = f"postgresql://postgres:{password}@db.{project_ref}.supabase.co:5432/postgres"
+DATABASE_URL = f"postgresql+psycopg2://postgres:{DB_PASSWORD}@localhost:5432/plenilune"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
